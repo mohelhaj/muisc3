5f46742e5ce4c40fd6197fab937a8f953219ad24
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-30 15:17:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -342,7 +342,7 @@ client.on('message', message => {
     }
 });
 
-const developers = ["462437279487754240"]
+const developers = ["462437279487754240","442670807240671252",""]
 const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
